5fe8ceca54f160de6df97f9b7691d6efb546a8d7
carina-ds <carina.korcel@gmail.com>
2026-02-10 09:34:49 -0500

    Add files via upload

--- src/models/baselines.py
+++ src/models/baselines.py
@@ -1,1 +1,6 @@
 
+# Simple extractive baseline
+
+def lead_k(text: str, k: int = 3) -> str:
+    sentences = text.split('.')
+    return '.'.join(sentences[:k]).strip() + '.'
---
+++ src/models/transformers.py
@@ -0,0 +1,7 @@
+
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+def load_model_and_tokenizer(model_name: str):
+    tok = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    return tok, model
